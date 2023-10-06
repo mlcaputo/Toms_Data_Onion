1da957accc2fae039aae3b3e9161eb4ce44c6430
Matt Caputo <mlcaputo98@gmail.com>
2023-10-06 14:23:33 -0500

    Removal of bit array code

--- TomsDataOnion/ascii85Decoder.cc
+++ TomsDataOnion/ascii85Decoder.cc
@@ -6,19 +6,6 @@
 #include <stdio.h>
 using namespace std;
 
-uint8_t binArrayToInt(uint8_t binArray[32], int positionNum){
-    int output = 0;
-    int power = 128;
-
-    for(int index = 8 * positionNum; index < (8 * (positionNum + 1)); index++){
-        output += binArray[index] * power;
-        power = power / 2;
-    }
-
-    return output;
-}
-
-
 string ascii85Decoder(std::string payloadBase){
 
     string result = "";
@@ -32,6 +19,7 @@ string ascii85Decoder(std::string payloadBase){
     int moddedSize = 0;
     moddedSize = payloadSize % 5;
 
+    // Adds padding for payload
     if(moddedSize != 0){
         for(int i = 0; i < moddedSize; i++){
             payloadBase += "u";
@@ -39,10 +27,12 @@ string ascii85Decoder(std::string payloadBase){
         }
     }
 
+    
     int i = 0;
     while(i <= payloadSize - 5){
         string payLoadChunk  = payloadBase.substr(i, 5);
 
+        // Checks for special character 'z'
         if(payLoadChunk[0] == 'z'){
             payLoadChunk = "!!!!!";
             payloadBase += "u";
@@ -54,6 +44,7 @@ string ascii85Decoder(std::string payloadBase){
         }
 
         uint32_t chunkInt = 0;
+        uint8_t buffer[4];
         uint8_t binArrayOfChunk[32];
     
         int symbol1 = int(payLoadChunk[0]) -33;
@@ -62,24 +53,21 @@ string ascii85Decoder(std::string payloadBase){
         int symbol4 = int(payLoadChunk[3]) -33;
         int symbol5 = int(payLoadChunk[4]) -33;
 
+        // Convert to base 85
         symbol1 = symbol1 * 52200625;
         symbol2 = symbol2 * 614125;
         symbol3 = symbol3 * 7225;
         symbol4 = symbol4 * 85;
-        
         chunkInt = symbol1 + symbol2 + symbol3 + symbol4 + symbol5;
         
-        int k = 31;
-        for (int j = 0; j < 32; ++j) {
-            binArrayOfChunk[k] = (chunkInt >> j) & 1;
-            k--;
-        }
-
+        // Separates 32 bit int into individual bytes
+        buffer[0] = (chunkInt >> 24) & 0xFF;
+        buffer[1] = (chunkInt >> 16) & 0xFF;
+        buffer[2] = (chunkInt >> 8) & 0xFF;
+        buffer[3] = chunkInt & 0xFF;
         
         for(int j = 0; j < 4; j++){
-            uint8_t temp  = 0;
-            temp = binArrayToInt(binArrayOfChunk, j);
-            result += (unsigned char) temp;
+            result += (unsigned char) buffer[j];
         }
     }
 
