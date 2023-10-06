b375a32869c29a46a331585f33f27119290719d1
Matt Caputo <mlcaputo98@gmail.com>
2023-10-06 15:30:59 -0500

    Replace bin arrays with bit wise operations.

--- TomsDataOnion/bitOperations.cc
+++ TomsDataOnion/bitOperations.cc
@@ -4,58 +4,20 @@
 #include <iostream>
 #include <Bits.h>
 #include <stdio.h>
+#include <climits>
 
 using namespace std;
 
-uint8_t binArrayToInt(uint8_t binArray[8]){
-    int output = 0;
-    int power = 128;
-
-    for(int index = 0; index < 8; index++){
-        output += binArray[index] * power;
-        power = power / 2;
-    }
-
-    return output;
-}
 
 uint8_t flipEveryOther(uint8_t num){
-    uint8_t binArray[8];
-    int k = 7;
-    for (int j = 0; j < 8; ++j) {
-        binArray[k] = (num >> j) & 1;
-        k--;
-    }
-
-    for(int i = 0; i < 8; i++){
-        if(i % 2 != 0){
-            binArray[i] = !binArray[i];
-        }
-    }
-
-    return binArrayToInt(binArray);
+    uint8_t mask = 85; // 0101 0101
+    num ^= mask;       // num XOR mask 
+    return num;
 
 }
 
 uint8_t rotateRight(uint8_t num){
-    uint8_t binArray[8];
-    int k = 7;
-    for (int j = 0; j < 8; ++j) {
-        binArray[k] = (num >> j) & 1;
-        k--;
-    }
-
-    uint8_t hold = binArray[7];
-    for(int i = 7; i >= 0; i--){
-        if(i == 0){
-            binArray[i] = hold;
-        }
-        else{
-            binArray[i] = binArray[i - 1];
-        }
-    }
-
-    return binArrayToInt(binArray);
+    return (num >> 1) | (num << 7); // OR combines right shift(1x) and left shifts(7x)
 }
 
 string bitwiseOperations(string input){
@@ -67,7 +29,6 @@ string bitwiseOperations(string input){
         uint8_t temp = (uint8_t) input[i];
         temp = flipEveryOther(temp);
         temp = rotateRight(temp);
-
         result += (unsigned char) temp; 
 
     }
