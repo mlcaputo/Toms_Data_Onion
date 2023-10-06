3adc4c46a25548c0dd8c0d87f450dadfe1e5369a
Matt Caputo <mlcaputo98@gmail.com>
2023-10-06 16:31:12 -0500

    Update paritybit functions to use bit operations.

--- TomsDataOnion/parityBitOperations.cc
+++ TomsDataOnion/parityBitOperations.cc
@@ -9,60 +9,34 @@ using namespace std;
 
 string removeParityBit(uint8_t input[8]){
     string result = "";
-    uint8_t totalBitArray[56];
     
-    for(int j = 0; j < 8; j++){
-        uint8_t tempBinArray[8];
-        int k = 7;
-        for (int x = 0; x < 8; ++x) {
-            tempBinArray[k] = (input[j] >> x) & 1;
-            k--;
-        }
+    for (int j = 0; j < 7; j++) {
+        input[j] = input[j] >> 1; // Shift right and left once to remove parity bit
+        input[j] = input[j] << 1;
 
-        int index = 0;
-        while(index < 7){
-            totalBitArray[index + (j * 7)] = tempBinArray[index];
-            index++;
-        }   
+        input[j] = (input[j] << j) | (input[j + 1] >> (7 - j));
+        result += (unsigned char) input[j];
     }
-
-    for(int j = 0; j < 7; j++){
-        int output = 0;
-        int power = 128;
-
-        for(int index = 0; index < 8; index++){
-            output += totalBitArray[index + (j * 8)] * power;
-            power = power / 2;
-        }
-
-        result += (unsigned char) output;
-    }
-
+  
     return result;
 }
 
 bool checkParityBit(uint8_t num){
-    uint8_t binArray[8];
     int countOfOnes = 0;
-    int k = 7;
-    for (int j = 0; j < 8; ++j) {
-        binArray[k] = (num >> j) & 1;
-        if(binArray[k] == 1 && k != 7){
-            countOfOnes++;
-        }
-        k--;
+
+    while (num != 0) {
+        num = num & (num - 1); // Removes a sig 1 with each pass
+        countOfOnes++;
     }
 
-    if(countOfOnes % 2 == 0 && binArray[7] == 0){
+    if(countOfOnes % 2 == (num & (1 << 1)) ){
+        return true;
+    }
+    if(countOfOnes % 2 == (num & (1 << 1)) ){
         return true;
     }
 
-    if(countOfOnes % 2 == 1 && binArray[7] == 1){
-        return true;
-    }
-
-    return false;
-
+    return false;      
 }
 
 string parityBitOperation(string input){
